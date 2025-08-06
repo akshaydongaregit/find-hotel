36b1e36b71a04aa2eb575dfc7fb3f72c9ef485aa
Akshay Dongare <34915737+akshaydongaregit@users.noreply.github.com>
2025-08-06 14:42:52 +0530

    Create HotelFinderService.java

---
+++ java/service/HotelFinderService.java
@@ -0,0 +1,12 @@
+package main.service;
+
+import main.rate.HotelRateStrategy;
+import main.dao.Hotel;
+
+/**
+ * @author akshaydongare
+ */
+public interface HotelFinderService {
+
+    public Hotel findHotel();
+}
