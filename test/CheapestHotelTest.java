944b468e52c3b660825f03696cb7134b7cc03ff5
Akshay Dongare <34915737+akshaydongaregit@users.noreply.github.com>
2025-08-06 14:44:15 +0530

    Create CheapestHotelTest.java

---
+++ test/CheapestHotelTest.java
@@ -0,0 +1,35 @@
+
+import main.dao.Hotel;
+import main.ReservationSystem;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class CheapestHotelTest {
+
+    @Test
+    public void findCheapestHotel(){
+        Hotel MiamiBeach = new Hotel(80, "Miami Beach");
+        Hotel MiamiDowntown = new Hotel(120, "Miami Downtown");
+        Hotel MiamiMidtown = new Hotel(100, "Miami Midtown");
+        List<Hotel> hotelList = new ArrayList<>();
+        hotelList.add(MiamiDowntown);
+        hotelList.add(MiamiBeach);
+        hotelList.add(MiamiMidtown);
+        ReservationSystem reservationSystem = new ReservationSystem(hotelList);
+        Hotel cheapestHotel = reservationSystem.findCheapestHotel();
+        assertEquals(MiamiBeach, cheapestHotel);
+    }
+
+    @Test
+    public void findCheapestHotelInEmptyList() {
+        List<Hotel> hotelList = new ArrayList<>();
+        ReservationSystem reservationSystem = new ReservationSystem(hotelList);
+        Hotel cheapestHotel = reservationSystem.findCheapestHotel();
+        assertNull(cheapestHotel);
+    }
+}
